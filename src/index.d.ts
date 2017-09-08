95618892214b3a5ec31352ba910665143be85c98
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-09-08 15:40:21 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,37 +4,39 @@
  * Definitions by: Alberto Schiabel <https://github.com/jkomyno>
  */
 
-import { PureComponent } from 'react';
-import { ViewPropTypes } from 'react-native';
+declare module 'react-native-horizontal-progress-bar' {
+  import { PureComponent } from 'react';
+  import { ViewPropTypes } from 'react-native';
+  
+  export interface HorizontalProgressBarProps {
+    /**
+     * Current progress of the <ProgressBar /> component
+     */
+    progress: number,
+    /**
+     * If set to false, the animation will be disabled
+     * 
+     * Default is true
+     */
+    animated?: boolean,
+    /**
+     * Background color of the progress bar
+     * 
+     * Default is '#7D85BE'
+     */
+    color?: string,
+    /**
+     * Duration of the transition between the current progress and the next one.
+     * Considered only if animated === true
+     * 
+     * Default is 500
+     */
+    duration: number,
+    /**
+     * Style of the ProgressBar container View
+     */
+    style?: ViewPropTypes.style,
+  }
 
-export interface HorizontalProgressBarProps {
-  /**
-   * Current progress of the <ProgressBar /> component
-   */
-  progress: number,
-  /**
-   * If set to false, the animation will be disabled
-   * 
-   * Default is true
-   */
-  animated?: boolean,
-  /**
-   * Background color of the progress bar
-   * 
-   * Default is '#7D85BE'
-   */
-  color?: string,
-  /**
-   * Duration of the transition between the current progress and the next one.
-   * Considered only if animated === true
-   * 
-   * Default is 500
-   */
-  duration: number,
-  /**
-   * Style of the ProgressBar container View
-   */
-  style?: ViewPropTypes.style,
+  export default class HorizontalProgressBar extends PureComponent<HorizontalProgressBarProps, null> {}
 }
-
-export default class HorizontalProgressBar extends PureComponent<HorizontalProgressBarProps, null> {}
